62951a667613ce4089f8b284e1a26e9f28023ba5
introduccion02 <introduccion02@lmint22.iio2.itam.mx>
2015-02-17 12:59:10 -0600

    se agrego metodo que dicwe si es palindromo o no

--- src/stackadt/Playground.java
+++ src/stackadt/Playground.java
@@ -19,10 +19,12 @@ public class Playground {
         miPila s = new miPila();
         Scanner lec = new Scanner(System.in);
         char ent;
-        String ent2, evaluacion="Todo bien";
+        String ent2, evaluacion="Todo bien. ☺";
+        StringBuilder cad = new StringBuilder();
+        StringBuilder cad2 = new StringBuilder();
         
         System.out.print("Ingresa un String para ser evaluado.\n");
-        ent2=lec.next();
+        ent2=lec.nextLine();
         for(int i=0;i<ent2.length();i++){
             ent=ent2.charAt(i);
             if(ent=='('){
@@ -30,7 +32,7 @@ public class Playground {
             }
             else if(ent==')'){
                 if(s.isEmpty()){
-                    evaluacion="Tiene parentesis mal puestos.";
+                    evaluacion="Tiene parentesis mal puestos. ▓▓▒▒▒▓▓";
                 }
                 else{
                 s.pop();
@@ -38,8 +40,30 @@ public class Playground {
             }
         }
         if(!s.isEmpty()){
-            evaluacion="Tiene parentesis mal puestos.";
+            evaluacion="Tiene parentesis mal puestos. ▓▓▒▒▒▓▓";
         }
         System.out.print("\n"+evaluacion);
+        
+        System.out.print("\nIngresa un String para ser evaluado.\n");
+        ent2=lec.nextLine();
+         for(int i=0;i<ent2.length();i++){
+             ent=ent2.charAt(i);
+             if(ent!=' '){
+                cad2.append(ent);
+             }
+             s.push(ent);
+         }
+         for(int i=0;i<ent2.length();i++){
+             if(s.peek()!=' '){
+                cad.append(s.peek());
+             }
+             s.pop();
+         }
+         if(cad2.toString().equals(cad.toString())){
+              System.out.print("\nEs un palindromo‼ ☻☺☻☺☻\n");
+         }
+         else{
+              System.out.print("\nNo tienes un palindromo ▓▓▒▒▒▓▓\n");
+         }
     }
 }
